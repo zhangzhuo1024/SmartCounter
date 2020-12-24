1e8828b88a3179e29ab6f283beb7e7cc8b4dc057
zhuozhang6 <zhuozhang6@iflytek.com>
2020-12-24 21:25:04 +0800

    添加开始时间、结束时间

--- app/src/main/java/com/quarkboom/smartcounter/MainActivity.java
+++ app/src/main/java/com/quarkboom/smartcounter/MainActivity.java
@@ -1,17 +1,25 @@
 package com.quarkboom.smartcounter;
 
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends BaseActivity {
-    private TextView mCounterNumber;
+    private TextView mCounterNumberText;
     private MyRecyclerViewAdapter myRecyclerViewAdapter;
     private int counter = 0;
     ArrayList<CounterBean> counterBeanList = new ArrayList<>();
+    private String mStartTime;
+    private String mEndTime;
 
     @Override
     protected int getLayoutId() {
@@ -24,19 +32,64 @@ public class MainActivity extends BaseActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         myRecyclerViewAdapter = new MyRecyclerViewAdapter();
         recyclerView.setAdapter(myRecyclerViewAdapter);
-        mCounterNumber = ((TextView) findViewById(R.id.counter_number));
+        mCounterNumberText = ((TextView) findViewById(R.id.counter_number));
         findViewById(R.id.add).setOnClickListener(view -> {
+            if (counter == 0) {
+                mStartTime = getLocalTime();
+            }
             counter++;
-            mCounterNumber.setText(counter + "");
+            mCounterNumberText.setText(counter + "");
         });
         findViewById(R.id.reset).setOnClickListener(view -> {
-            CounterBean counterBean = new CounterBean();
-            counterBean.setmName("1111");
-            counterBean.setmTotal(counter + "");
-            counterBeanList.add(counterBean);
-            myRecyclerViewAdapter.updateData(counterBeanList);
-            counter = 0;
+            if (counter > 0) {
+                mEndTime = getLocalTime();
+                CounterBean counterBean = new CounterBean();
+                counterBean.setmName("1111");
+                counterBean.setmTotal(counter + "次");
+                counterBean.setmStartTime(mStartTime);
+                counterBean.setmEndTime(mEndTime);
+                counterBeanList.add(counterBean);
+                myRecyclerViewAdapter.updateData(counterBeanList);
+                counter = 0;
+                mCounterNumberText.setText(counter + "");
+            }
         });
+    }
+
+
+    private String getNetTime() {
+        URL url = null;//取得资源对象
+        try {
+            url = new URL("http://www.baidu.com");
+            //url = new URL("http://www.ntsc.ac.cn");//中国科学院国家授时中心
+            //url = new URL("http://www.bjtime.cn");
+            URLConnection uc = url.openConnection();//生成连接对象
+            uc.connect(); //发出连接
+            long ld = uc.getDate(); //取得网站日期时间
+            DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(ld);
+            String format = formatter.format(calendar.getTime());
+            return format;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    private String getLocalTime() {
+        URL url = null;//取得资源对象
+        try {
+            DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(System.currentTimeMillis());
+            String format = formatter.format(calendar.getTime());
+            Toast.makeText(this, "当前系统时间为: \n" + format, Toast.LENGTH_SHORT).show();
+            return format;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 
 }
